0dc9532d968f045e5b234228248e6e05632fff15
Muhammad Shaheer <shaheerkhan0012345@gmail.com>
2026-08-07 21:41:29 -0700

    Revert "style: shift hero right text further right and increase responsive tooth size"
    This reverts commit ef8e9dde8b800dce90580e7006c8db699b189d62.

--- src/components/dashboard/tabs/QueueTab.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { QueueBoard } from '../../queue/QueueBoard';
-
-export const QueueTab: React.FC = () => {
-  return <QueueBoard />;
-};
--- src/components/dashboard/tabs/ReportsTab.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ReportGenerator } from '../../reports/ReportGenerator';
-
-export const ReportsTab: React.FC = () => {
-  return <ReportGenerator />;
-};
